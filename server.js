337190415b406a003becdf65fea3d5aab315f029
Praveen1104 <impraveenbosco@gmail.com>
2026-05-21 12:40:05 +0530

    Initial docker ci/cd setup

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World CI/CD Working!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
